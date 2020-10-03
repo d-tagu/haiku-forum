06c5216f2ff0ab98bc5d101bcb4bc59ea10f5503
daikiiita <daikitag01@gmail.com>
2020-10-03 10:43:26 +0900

    Add: router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import PageHome from "./components/PageHome";
+import PageThread from "./components/PageThread";
+import PageNotFound from "./components/PageNotFound";
+import { createMuiTheme, CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
 
-function App() {
+const App = () => {
+  const theme = createMuiTheme();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route exact path={"/"}>
+              <PageHome />
+            </Route>
+            <Route exact path={"/threads/:threadId"}>
+              <PageThread />
+            </Route>
+            <Route path={"*"}>
+              <PageNotFound />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
-}
+};
 
 export default App;
